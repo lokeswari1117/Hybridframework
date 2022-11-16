cb32061aec3c244fe8d9b4ee2aae1a479612e385
user <user@user-PC>
2022-11-16 17:26:58 +0530

    move to cloud

--- Hybridframework/src/test/java/com/testscenarios/Fb_login.java
+++ Hybridframework/src/test/java/com/testscenarios/Fb_login.java
@@ -1,24 +1,53 @@
 package com.testscenarios;
 
-import org.testng.annotations.Test;
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class Fb_login {
+	WebDriver driver;
   @Test
-  public void f() {
+  public void f() throws Exception {
+	  
+	  driver.get("https://www.facebook.com");
+	  driver.findElement(By.name("email")).sendKeys("swathi");
+  	driver.findElement(By.name("pass")).sendKeys("chowdhary");
+  	Thread.sleep(5000);
+		driver.findElement(By.tagName("button")).click();
   }
   @AfterMethod
-  public void afterMethod() {
+  public void afterMethod() throws Exception {
+		 Date d = new Date();
+		 DateFormat abcd = new SimpleDateFormat("ddMMYYYY_HHmmss");
+		 String timeStamp = abcd.format(d);
+		File abc = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	    FileHandler.copy(abc, new File(".\\screenshots\\test_"+timeStamp+".png"));
   }
 
   @BeforeClass
   public void beforeClass() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();	 
   }
 
   @AfterClass
   public void afterClass() {
+	  driver.quit();
   }
 
 }
